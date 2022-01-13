cff9fd9655d13b7f4eb936664e0f65b54ea3a656
Brian Foster <brianisbrilliant@gmail.com>
2022-01-13 16:36:34 -0600

    Built a level just from cubes

---
+++ Assets/Scripts/TreeRandomizer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeRandomizer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // randomize rotation on vertical axis
+        this.transform.Rotate(0,Random.Range(-180f,180f),0);
+        // randomize value of current color.
+        Renderer rend = this.GetComponent<Renderer>();
+        float h, s, v;
+
+        Color.RGBToHSV(rend.material.color, out h, out s, out v);
+        Color newColor = Color.HSVToRGB(h, s, Random.Range(0.4f,0.9f));
+        rend.material.color = newColor;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
